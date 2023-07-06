46bb3b0e04b49a37cbab320f56cbddb5385f118d
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-07-06 14:25:34 +0530

    cors updated

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
